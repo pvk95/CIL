5f85fe5c384c775e97366ce70700e6ba81b36e31
martin <vilmarti@student.ethz.ch>
2019-06-18 14:27:53 +0200

    changed to unet

--- scripts/run_model.sh
+++ scripts/run_model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Train the model first
-python scripts/main.py -save_folder UNet/ -arch unet -lr 0.01 -batch_sz 64
+python scripts/main.py -save_folder UNet/ -arch unet -lr 0.1 -batch_sz 64
 
 #Get predictions
-python scripts/results.py -save_folder UNet/
+python scripts/results.py -save_folder UNet/ -arch unet
 
