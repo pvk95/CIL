25c05411d1d4c528b7d3c06953e6dc343e832638
martin <vilmarti@student.ethz.ch>
2019-06-21 14:46:13 +0200

    removed pdb

--- scripts/main.py
+++ scripts/main.py
@@ -41,7 +41,6 @@ def produceSegmentedImages(model, X_test, save_folder, mode=0):
         reconstruct_from_patches = utils.from_padding
 
     X_patches = produce_patches(X_test)
-    import pdb; pdb.set_trace()
     X_patches = np.array(X_patches)
     y_patches = model.predict(X_patches)
     y_pred = reconstruct_from_patches(y_patches)
