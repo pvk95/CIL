ceb227e4c38af2a0477d2b79b4b4eb949c2413b8
martin <vilmarti@student.ethz.ch>
2019-06-21 14:09:45 +0200

    change run_model to unet

--- scripts/run_model.sh
+++ scripts/run_model.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #Train the model first
-python3 scripts/main.py -save_folder Combined/ -arch combined -lr 0.01 -batch_sz 32 -epochs 15 -rec_mode 2
+python3 scripts/main.py -save_folder UNet/ -arch unet -lr 0.01 -batch_sz 32 -epochs 15 -rec_mode 2
 
 #Get predictions
-python3 scripts/results.py -save_folder Combined/ -arch combined 
+python3 scripts/results.py -save_folder UNet/ -arch unet 
 
