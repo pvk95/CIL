be40661d0c9cbdee86e0da1c50cefe39545fba1d
martin <vilmarti@student.ethz.ch>
2019-06-21 11:12:36 +0200

    increased batch_size

--- scripts/run_model.sh
+++ scripts/run_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Train the model first
-python3 scripts/main.py -save_folder Combined/ -arch combined -lr 0.01 -batch_sz 1 -epochs 10 -rec_mode 1
+python3 scripts/main.py -save_folder Combined/ -arch combined -lr 0.01 -batch_sz 32 -epochs 10 -rec_mode 1
 
 #Get predictions
 python3 scripts/results.py -save_folder Combined/ -arch combined 
